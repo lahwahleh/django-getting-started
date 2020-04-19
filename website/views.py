507035039012d4c890c6f3f4592dd75a815f4a74
Olawale Ayodele <olawaleayodele@gmail.com>
2020-04-19 16:33:40 +0100

    Link building

--- website/views.py
+++ website/views.py
@@ -3,8 +3,7 @@ from django.http import HttpResponse
 from datetime import datetime
 # from datetime import date
 # from datetime import today
-from meetings.models import Meeting
-
+from meetings.models import Meeting, Room
 
 
 # Create your views here.
@@ -12,8 +11,15 @@ from meetings.models import Meeting
 # def welcome(request):
 #     return HttpResponse("Welcome to the Meeting Planner")
 
+# def welcome(request):
+#     return render(request, "website/welcome.html", {"num_meetings": Meeting.objects.count()})
+
 def welcome(request):
-    return render(request, "website/welcome.html", {"num_meetings": Meeting.objects.count()})
+    return render(request, "website/welcome.html", {"meetings": Meeting.objects.all()})
+
+
+
+
 
 
 def date(request):
@@ -21,5 +27,5 @@ def date(request):
 
 
 def about(request):
-   # return HttpResponse("I am Olawale. I'm glad to be alive today " + str(datetime.date.today()) )
-    return HttpResponse("I am Olawale. I'm glad to be alive today " )
+    # return HttpResponse("I am Olawale. I'm glad to be alive today " + str(datetime.date.today()) )
+    return HttpResponse("I am Olawale. I'm glad to be alive today ")
