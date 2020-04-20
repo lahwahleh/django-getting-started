d8259557b628dcd582f83dd5fe6628f746085679
Olawale Ayodele <olawaleayodele@gmail.com>
2020-04-20 17:41:03 +0100

    Modelform

--- meetings/views.py
+++ meetings/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.forms import modelform_factory
 from .models import Meeting, Room
 
 # Create your views here.
@@ -12,5 +13,18 @@ def detail (request, id):
 def rooms(request):
     return render(request, "meetings/rooms.html",
                   {"rooms": Room.objects.all()})
+
+MeetingForm = modelform_factory(Meeting, exclude=[])
+
+def new(request):
+    if request.method == "POST":
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("welcome")
+
+    else:
+        form = MeetingForm()
+    return render(request, "meetings/new.html", {'form': form})
 
 
